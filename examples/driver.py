13ddf1f945844392bc4da183b8550ac9faa403e9
Zac Strei <Zacstrei88@gmail.com>
2026-01-19 11:17:10 -0700

    updated example driver script with q1

--- examples/driver.py
+++ examples/driver.py
@@ -1,12 +1,32 @@
-import numpy as np
-from goph547lab00.arrays import square_ones
+#import numpy as np
+#from goph547lab00.arrays import square_ones
 
-def main():
+#def main():
     # test creating square array of ones
-    A_np = np.ones((3,3))
-    A = square_ones(3)
-    print(f'A_np:\n{A_np}')
-    print(f'A:\n{A}')
+    #A_np = np.ones((3,3))
+    #A = square_ones(3)
+    #print(f'A_np:\n{A_np}')
+    #print(f'A:\n{A}')
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from goph547lab00.arrays import (
+    square_ones,
+)
+
+def arrays():
+
+    A_np = np.ones((3, 3))
+    A = square_ones(3)
+    print(f"A_np:\n{A_np}")
+    print(f"A:\n{A}")
+    print()
+
+    # Question B1
+    C = np.ones((3, 5))
+    print(f"(3, 5) array of ones\n{C}")
