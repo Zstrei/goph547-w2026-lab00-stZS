645bea8e73d00625989b08988e146bd550e00c85
Zac Strei <Zacstrei88@gmail.com>
2026-01-21 23:10:58 -0700

    updated driver.py with completed parts A and B of Lab 00

--- examples/driver.py
+++ examples/driver.py
@@ -1,16 +1,3 @@
-#import numpy as np
-#from goph547lab00.arrays import square_ones
-
-#def main():
-    # test creating square array of ones
-    #A_np = np.ones((3,3))
-    #A = square_ones(3)
-    #print(f'A_np:\n{A_np}')
-    #print(f'A:\n{A}')
-
-#if __name__ == '__main__':
-    #main()
-
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
